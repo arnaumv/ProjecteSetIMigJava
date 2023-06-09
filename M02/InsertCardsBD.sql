b186b3f469f1ccfffcc9d57de2bacae07ca45caf
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-06-09 13:57:55 +0200

    Create InsertCardsBD.sql

---
+++ M02/InsertCardsBD.sql
@@ -0,0 +1,89 @@
+
+insert into card (card_id,card_name,card_value,card_real_value)
+values('01','oros',1,1);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('02','oros',2,2);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('03','oros',3,3);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('04','oros',4,4);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('05','oros',5,5);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('06','oros',6,6);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('07','oros',7,7);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('08','oros',0.5,8);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('09','oros',0.5,9);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('10','oros',0.5,10);
+select * from card;
+
+insert into card (card_id,card_name,card_value,card_real_value)
+values('11','bastos',1,1);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('12','bastos',2,2);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('13','bastos',3,3);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('14','bastos',4,4);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('15','bastos',5,5);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('16','bastos',6,6);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('17','bastos',7,7);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('18','bastos',0.5,8);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('19','bastos',0.5,9);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('20','bastos',0.5,10);
+
+
+
+insert into card (card_id,card_name,card_value,card_real_value)
+values('21','espadas',1,1);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('22','espadas',2,2);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('23','espadas',3,3);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('24','espadas',4,4);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('25','espadas',5,5);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('26','espadas',6,6);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('27','espadas',7,7);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('28','espadas',0.5,8);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('29','espadas',0.5,9);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('30','espadas',0.5,10);
+
+
+
+insert into card (card_id,card_name,card_value,card_real_value)
+values('31','copas',1,1);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('32','copas',2,2);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('33','copas',3,3);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('34','copas',4,4);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('35','copas',5,5);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('36','copas',6,6);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('37','copas',7,7);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('38','copas',0.5,8);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('39','copas',0.5,9);
+insert into card (card_id,card_name,card_value,card_real_value)
+values('40','copas',0.5,10);
